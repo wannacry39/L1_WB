cbcd5cb9c4603831fbe30ad28cfbd91c9e01679f
wncry <emailexample@gmail.com>
2024-06-17 00:04:55 +0300

    new tasks

--- task21/t21.go
+++ task21/t21.go
@@ -1,5 +1,36 @@
 package main
 
+import "fmt"
+
+type Driver struct {
+	name string
+}
+
+type Pilot struct {
+	name string
+}
+
+type Adapter struct {
+	Driver
+}
+
+type fly interface {
+	Fly()
+}
+
+func (a *Adapter) Fly() {
+	fmt.Println("Flying on the plain")
+}
+
+func (d *Driver) drive() {
+	fmt.Println("driving a car")
+}
+
 func main() {
+	ob := Driver{"Oleg"}
+	ob.drive()
+
+	ad := Adapter{ob}
+	ad.Fly()
 
 }
